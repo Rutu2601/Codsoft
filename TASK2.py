7dc2dac8910e7b53b6f9d6c415a8451c6f69f1dc
Rutu2601 <168261359+Rutu2601@users.noreply.github.com>
2024-06-13 09:51:44 +0530

    Update TASK2.py

--- TASK2.py
+++ TASK2.py
@@ -6,11 +6,9 @@ def button_click(entry, text):
     entry.delete(0, tk.END)
     entry.insert(0, current_text + text)
 
-# Function to clear the entry box
 def clear(entry):
     entry.delete(0, tk.END)
 
-# Function to evaluate the expression in the entry box
 def evaluate(entry):
     try:
         result = str(eval(entry.get()))
@@ -21,16 +19,12 @@ def evaluate(entry):
         entry.insert(0, "Error")
 
 def create_calculator():
-    # Create the main window
     root = tk.Tk()
     root.title("Calculator")
     root.geometry("300x400")
-
-    # Create an entry widget for the display
     entry = tk.Entry(root, font=("Arial", 20), bd=10, insertwidth=2, width=14, borderwidth=4)
     entry.grid(row=0, column=0, columnspan=4)
 
-    # Button labels
     button_labels = [
         '7', '8', '9', '/',
         '4', '5', '6', '*',
@@ -52,12 +46,9 @@ def create_calculator():
             col_val = 0
             row_val += 1
 
-    # Clear button
     clear_button = tk.Button(root, text='C', padx=20, pady=20, font=("Arial", 18), command=partial(clear,entry))
     clear_button.grid(row=row_val, column=0, columnspan=4)
 
-    # Run the application
     root.mainloop()
-
-# Run the calculator application
+    
 create_calculator()
